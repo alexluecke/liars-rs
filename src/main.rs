91d3e27391e556531a23fd85a198399d73c47888
Alex Luecke <me@alexjl.com>
2015-04-19 13:15:15 -0700

    Maybe finished

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,33 @@
 extern crate rand;
 use rand::{thread_rng, Rng};
 use std::num::Float;
+use std::fmt;
 
-enum Person {
+enum Type {
     Liar,
     Honest,
 }
 
-fn ask_if_liar(x: &Person, y: &Person) -> bool {
-    match *x {
-        Person::Honest => match *y {
-                Person::Liar => true,
-                _ => false,
-            },
-        _ => generate_bool()
+struct Person {
+    integrity: Type,
+}
+
+impl fmt::Debug for Person {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self.integrity {
+            Type::Liar => write!(f, "Liar"),
+            _ => write!(f, "Honest")
+        }
     }
 }
 
-fn generate_random_person() -> Person {
-    match generate_bool {
-        true => Person::Honest,
-        _ => Person::Liar,
+fn ask_if_liar(x: &Person, y: &Person) -> bool {
+    match (*x).integrity {
+        Type::Honest => match (*y).integrity {
+            Type::Liar => true,
+            _ => false,
+        },
+        _ => generate_bool()
     }
 }
 
@@ -30,69 +37,120 @@ fn generate_bool() -> bool {
 
 fn main() {
     let num_people = 9;
-    let mut max_liars = num_people/2;
+
+    let mut max_liars: isize = match num_people%2 {
+        0 => num_people/2 - 1,
+        _ => num_people/2
+    };
+    let max = max_liars;
+
     let mut set_a: Vec<Person> = Vec::new();
     let mut set_b: Vec<Person> = Vec::new();
-    let mut d: &Person;
 
     let mut people: Vec<Person> = (0..num_people)
-        .map(|_| match generate_random_person() {
-            Person::Honest => Person::Honest,
+        .map(|_| match generate_bool() {
+            true => Person { integrity: Type::Honest },
             _ => {
-                    max_liars -= 1;
-                    match max_liars > 0 {
-                        true => Person::Liar,
-                        _ => Person::Honest
-                    }
-                },
+                max_liars -= 1;
+                match max_liars > 0 {
+                    true => Person { integrity: Type::Liar },
+                    _ => Person { integrity: Type::Honest }
+                }
+            },
         })
-        .collect();
+    .collect();
 
 
-   while people.len() > 0 {
+    println!("People: ");
+    for person in people.iter().rev() {
+        match (*person).integrity {
+            Type::Liar => print!("Liar, "),
+            _ => print!("Honest, "),
+        }
+    }
+    println!("\n");
 
-       let x = people.pop();
-       let y = if set_a.len() > 0 {
-         set_a.pop()
-       } else {
-         people.pop()
-       };
+    let mut count = 0;
+    while people.len() > 0 {
 
-       match (x, y) {
-           (Some(x), Some(y)) => {
-               if ask_if_liar(&x, &y) {
-                   set_b.push(x);
-                   set_b.push(y);
-               } else {
-                   set_a.push(x); // push x first!
-                   set_a.push(y);
-               }
-           }
-           (Some(x), _) => println!("One more"),
-           _ => println!("done")
-       }
-   }
+        let x = people.pop();
+        let y = if set_a.len() > 0 {
+            set_a.pop()
+        } else {
+            people.pop()
+        };
 
-   for person in set_a.iter() {
-       match *person {
-           Person::Liar => println!("Liar"),
-           _ => println!("Honest"),
-       }
-   }
+        match (x, y) {
+            (Some(x), Some(y)) => {
+                if ask_if_liar(&x, &y) {
+                    println!("({:?}) says ({:?}) is liar => put in set B.", x, y);
+                    set_b.push(x);
+                    set_b.push(y);
+                } else {
+                    println!("({:?}) says ({:?}) is honest => put in set A.", x, y);
+                    set_a.push(x); // push x first!
+                    set_a.push(y);
+                }
+            }
+            (Some(x), _) => println!(""),
+            _ => println!("")
+        }
 
-   println!("--------------------------------------------------");
+        count += 1;
+        println!("round {}, set_a.let(): {}, max: {}", count, set_a.len(), max);
 
-   for person in set_b.iter() {
-       match *person {
-           Person::Liar => println!("Liar"),
-           _ => println!("Honest"),
-       }
-   }
+        if set_a.len() as isize > max {
+            println!("\nSet A grew too big");
+            match set_a.pop() {
+                Some(x) => match x.integrity {
+                    Type::Honest => println!("DM was honest"),
+                    _ => println!("DM was a liar"),
+                },
+                None => println!("Oh boy, this should not have happened."),
+            }
+            break;
+        }
 
-    //for person in people.iter() {
-        //match *person {
-            //Person::Liar => println!("Liar"),
-            //_ => println!("Honest"),
-        //}
-    //}
+        if set_b.len() as isize >= 2*max {
+            println!("\nSet B grew too big");
+            if set_a.len() > 0 {
+                match set_a.pop() {
+                    Some(x) => match x.integrity {
+                        Type::Honest => println!("DM was honest"),
+                        _ => println!("DM was a liar"),
+                    },
+                    None => println!("Oh boy, this should not have happened."),
+                }
+            } else {
+                match people.pop() {
+                    Some(x) => match x.integrity {
+                        Type::Honest => println!("DM was honest"),
+                        _ => println!("dm was a liar"),
+                    },
+                    None => println!("Oh boy, this should not have happened."),
+                }
+            }
+            break;
+        }
+    }
+
+    println!("\n--------------------------------------------------");
+    println!("Set A");
+    for person in set_a.iter() {
+        match (*person).integrity {
+            Type::Liar => println!("Liar"),
+            _ => println!("Honest"),
+        }
+    }
+
+    println!("\n--------------------------------------------------");
+    println!("Set B");
+
+    for person in set_b.iter() {
+        match (*person).integrity {
+            Type::Liar => println!("Liar"),
+            _ => println!("Honest"),
+        }
+    }
+
 }
